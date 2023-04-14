6e915afbdb62225cead8dd6dee901c0a243aaada
Akshara789 <129933350+Akshara789@users.noreply.github.com>
2023-04-14 19:29:34 +0530

    Add files via upload

---
+++ Day8/Program1/Program1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first No:");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second No:");
+            int j = int.Parse(Console.ReadLine());
+            foo(i, j);
+            Console.WriteLine("debugging over");
+        }
+        public static void foo(int x,int y)
+        {
+            int t = 100;
+            for(int i=x;i<=y;i++) 
+            {
+                Console.WriteLine("India");
+                t = t + 34;
+            }
+        }
+        }
+    }
+
---
+++ Day8/String Upper/String Upper/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Upper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter the string:");
+            string str = Console.ReadLine();
+            char[] arr = str.ToCharArray();
+            
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if ((arr[i] >= 'a') && (arr[i] <= 'z'))
+                {
+                    Console.Write(char.ToUpper(arr[i]));
+                    
+                }
+                else if ((arr[i] >= 'A') && (arr[i] <= 'Z')) 
+                {
+                   Console.Write(char.ToLower(arr[i]));
+                   
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            
+            
+        }
+    }
+}
+            
+
+            
+
+
+        
+            
+    
+            
+             
