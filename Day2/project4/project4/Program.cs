61c5c8a1ea0a424a4d71148eff143480997b94ca
Akshara789 <129933350+Akshara789@users.noreply.github.com>
2023-04-14 17:48:16 +0530

    Add files via upload

---
+++ Day2/project4/project4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 0.0, Area;
+            Console.WriteLine("Enter radius");
+            radius = double.Parse(Console.ReadLine());
+            Area = AreaC(radius);
+            Console.WriteLine(Area);
+        }
+        static double AreaC(double radius)
+        {
+            double Area;
+            Area = 3.14 * radius * radius;
+            return Area;
+        } 
+
+        }
+    }
+
