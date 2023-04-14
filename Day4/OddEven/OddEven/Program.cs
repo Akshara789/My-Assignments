2d6c340ace7bb626da3049da70c403dce06c4f4c
Akshara789 <129933350+Akshara789@users.noreply.github.com>
2023-04-14 17:50:28 +0530

    Add files via upload

---
+++ Day4/OddEven/OddEven/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a[]
+            Console.WriteLine("Enter the number of elements in the array");
+            n=int.Parse(Console.ReadLine());
+
+        }
+    }
+}
---
+++ Day4/string asc/string asc/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_asc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            char[] arr;
+            char ch;
+            int i, j, l;
+            Console.WriteLine("Input the string");
+            str= Console.ReadLine();    
+            l= str.Length;  
+           arr = str.ToCharArray(0,l);
+            for(i=1;i<l;i++)
+                  for(j=0;j<l-i;j++)
+                     if (arr[j] > arr[j+1])
+                    {
+                        ch = arr[j]; 
+                        arr[j] = arr[j+1];
+                        arr[j+1] = ch; 
+                    }
+
+            Console.WriteLine("The string in ascending order is:");
+            foreach(char c in arr)
+              
+                {
+                    ch = c;
+                    Console.WriteLine(" {0} ",ch);
+                }
+            Console.WriteLine("\n");
+           
+        }
+    }
+}
