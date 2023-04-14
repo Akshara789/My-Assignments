b08bcf366444bfd8b30459c136ace6241581a2a1
Akshara789 <129933350+Akshara789@users.noreply.github.com>
2023-04-14 17:49:20 +0530

    Add files via upload

---
+++ Day3/Class1/Class1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1
+{
+    public class Class1
+    {
+        public int EmpId;
+        public string Name;
+        public decimal Salary;
+
+        public Class1()
+        {
+
+        }
+
+        public Class1(int empId, string name, decimal salary)
+        {
+            EmpId = empId;  
+            Name = name;
+            this.Salary = salary;
+        }
+    }
+}
---
+++ Day3/Class1/Class1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 500;
+            class1 e1 = new class1();
+            class1 e2 = new class1(300, "Rahul", 30000);
+            Console.WriteLine("Enter Employee Id:");
+            e1.EmpId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Employee Name:");
+            e1.Name = Console.ReadLine();
+            Console.WriteLine("Enter Employee Salary:");
+            e1.Salary = Decimal.Parse(Console.ReadLine());
+        }
+    }
+}
