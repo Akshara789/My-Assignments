10cfc42a9f2eabe2a106af8f5b4e58196105ca07
Akshara789 <129933350+Akshara789@users.noreply.github.com>
2023-05-02 23:29:32 +0530

    Add files via upload

---
+++ Day10/Indexer/Indexer/Index.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    public class array
+    {
+        float[]arr = new float[] {12.5f,34.5f,;  
+    }
+    {
+    }
+}
