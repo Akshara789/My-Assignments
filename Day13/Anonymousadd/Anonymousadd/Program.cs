0f7d76143fbd7d349f6a462fca67035ccf082b27
Akshara789 <129933350+Akshara789@users.noreply.github.com>
2023-05-03 15:01:25 +0530

    Add files via upload

---
+++ Day13/Anonymousadd/Anonymousadd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymousadd
+{
+    internal class Program
+    {
+        public delegate int sum(int x, int y);
+        static void Main(string[] args)
+        {
+            int result;
+            sum s = delegate (int a, int b)
+            {
+                return a + b;
+            };
+
+            result = s(10, 20);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Day13/SQLQuery2.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Person(
+  personID int primary key , LastName varchar(255), FirstName varchar(255),Address varchar(255),City varchar(255));
+  insert into person (personID,LastName,FirstName,Address,City)
+  values(234,'ks','ammu','hhjj','chennai')
+    
+  delete  from person;
+   insert into person (personID,LastName,FirstName,Address,City)
+  values(234,'ks','ammu','hhjj','chennai'),(345,'jh','ghkhk','hjkkf','amritha')
+  select * from person;
+    
