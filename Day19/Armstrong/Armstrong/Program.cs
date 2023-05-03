baed10f835afa0b1b53efcc181bf7335c84b9e13
Akshara789 <129933350+Akshara789@users.noreply.github.com>
2023-05-03 16:56:52 +0530

    Add files via upload

---
+++ Day19/Armstrong/Armstrong/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Armstrong
+{
+    internal class Program
+    {
+
+        static string armstrongNumber(int n)
+        {
+            int temp = 0, a = 0;
+            int b = n;
+            while (n > 0)
+            {
+                temp = n % 10;
+                a = (int)(a + Math.Pow(temp, 3));
+                n = n / 10;
+            }
+                if (b == a)
+                {
+                    return "yes";
+                }
+                else
+                {
+                    return "no";
+                }
+
+            
+        }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("ENTER NUMBER");
+               int  n = int.Parse(Console.ReadLine());
+                string result;
+                result = armstrongNumber(n);
+                Console.WriteLine(result);
+
+
+
+
+            }
+        
+    }
+}
+
---
+++ Day19/swap/swap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap
+{
+    internal class Program
+    {
+        static int swap(int a, int b) 
+        {
+            swap(a, b);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number");
+            int a=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter second number");
+            int b = int.Parse(Console.ReadLine());
+            int result=swap(a, b);
+            return result;
+
+        }
+    }
+}
