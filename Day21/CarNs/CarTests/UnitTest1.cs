4d3a85dd6f7c989de622f1f15352a36ec511dcdd
Akshara789 <129933350+Akshara789@users.noreply.github.com>
2023-05-07 11:11:53 +0530

    Add files via upload

---
+++ Day21/CarNs/CarTests/UnitTest1.cs
@@ -0,0 +1,37 @@
+namespace CarTests
+{
+    [TestClass]
+   /* public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }*/
+
+           public class CarTestClass
+    {
+        CarTestClass test_car;
+    }
+            public void CreateCarObject()
+            {
+                test_car = new Car("Toyota", "Prius", 10, 50);
+            }
+
+            //TODO: constructor sets gasTankLevel properly
+
+            //TODO: gasTankLevel is accurate after driving within tank range
+
+            //TODO: gasTankLevel is accurate after attempting to drive past tank range
+
+            //TODO: can't have more gas than tank size, expect an exception
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentOutOfRangeException))]
+            public void TestGasOverfillException()
+            {
+                test_car.AddGas(5);
+                Assert.Fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+            }
+        }
+    }
+    }
