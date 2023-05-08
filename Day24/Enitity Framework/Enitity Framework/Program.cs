23d0771acc880e54422a51bfb6cbf498c8d2efbd
Akshara789 <129933350+Akshara789@users.noreply.github.com>
2023-05-08 09:36:16 +0530

    Add files via upload

---
+++ Day24/Enitity Framework/Enitity Framework/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enitity_Framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EFDBEntities e1 = new EFDBEntities();
+
+            foreach (var item in e1.Depts)
+
+            {
+
+                Console.WriteLine(item.DeptName);
+
+            }
+
+            using (var context = new EFDBEntities())
+
+            {
+
+                var dept = new Dept()
+
+                {
+
+                    DeptId = 3,
+
+                    DeptName= "IT"
+
+                };
+
+                context.Depts.Add(dept);
+
+                context.SaveChanges();
+
+            }
+
+        }
+
+    }
+}
+    
+
